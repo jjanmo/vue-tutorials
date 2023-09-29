4fec9c37d2e6e690d31f97a8ec35fb4235382e00
jjanmo <jjanmo07@hanmail.net>
2023-09-29 19:51:41 +0900

    feat: initial commit and cleanup

---
+++ project01/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HelloWorld message="Hello World" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ project01/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    message: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
