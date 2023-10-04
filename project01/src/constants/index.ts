5eed274bc2334b0c2482119424cfd22c5c1ccffc
jjanmo <jjanmo07@hanmail.net>
2023-10-04 21:29:30 +0900

    feat: 홈페이지 컨텐츠 링크 컴퍼넌트 추가

---
+++ project01/src/constants/index.ts
@@ -0,0 +1,59 @@
+interface Link {
+  type: 'video' | 'document';
+  title: string;
+  url: string;
+  status: 'todo' | 'doing' | 'done';
+  version: 2 | 3;
+}
+
+export const links: Link[] = [
+  {
+    type: 'video',
+    title: '[개발자의 품격] 한시간만에 끝내는 Vue.js 입문',
+    url: 'https://www.youtube.com/watch?v=sqH0u8wN4Rs&list=PLqbWuGdVBJd3T1y8tVBdeuX-cDL3LEzt_&index=5&t=147s',
+    status: 'done',
+    version: 2,
+  },
+  {
+    type: 'video',
+    title: '[코지코더] Vue.js 2 기초 익히기 기본 강좌',
+    url: 'https://www.youtube.com/playlist?list=PLB7CpjPWqHOtYP7P_0Ls9XNed0NLvmkAh',
+    status: 'doing',
+    version: 2,
+  },
+  {
+    type: 'document',
+    title: '15 VUE JS Project Ideas: Beginner to Expert [With tutorial]',
+    url: 'https://nerdjfpb.medium.com/15-vue-js-project-ideas-beginner-to-expert-with-tutorial-9a315d184a91',
+    status: 'doing',
+    version: 2,
+  },
+  {
+    type: 'document',
+    title: 'Vue2 공식사이트',
+    url: 'https://v2.ko.vuejs.org/',
+    status: 'doing',
+    version: 2,
+  },
+  {
+    type: 'document',
+    title: 'Vue3 공식사이트',
+    url: 'https://v3-docs.vuejs-korea.org/',
+    status: 'todo',
+    version: 3,
+  },
+  {
+    type: 'video',
+    title: '[짐코딩] Vue3 완벽 마스터: 기초부터 실전까지 - "기본편"',
+    url: 'https://v3-docs.vuejs-korea.org/',
+    status: 'todo',
+    version: 3,
+  },
+  {
+    type: 'video',
+    title: '[짐코딩] Vue3 완벽 마스터: 기초부터 실전까지 - "실전편"',
+    url: 'https://v3-docs.vuejs-korea.org/',
+    status: 'todo',
+    version: 3,
+  },
+];
--- project01/src/views/Home.vue
+++ project01/src/views/Home.vue
@@ -1,29 +1,96 @@
 <template>
-  <div>
-    <div>{{ count }}</div>
-    <div>
-      <button @click="handleClickPlus">add</button>
-      <button @click="handleClickMinus">minus</button>
+  <div class="container">
+    <h1>내가 공부한 Vue 공부 자료들</h1>
+    <div class="button-container">
+      <button
+        :class="{ active: selected === 'all' }"
+        data-type="all"
+        @click="handleClick"
+      >
+        ALL
+      </button>
+      <button
+        :class="{ active: selected === 'v2' }"
+        data-type="v2"
+        @click="handleClick"
+      >
+        Version2
+      </button>
+      <button
+        :class="{ active: selected === 'v3' }"
+        data-type="v3"
+        @click="handleClick"
+      >
+        Version3
+      </button>
     </div>
+    <ul v-for="(link, i) of links" :key="i">
+      <li>
+        <span class="icon" v-if="link.status === 'done'">
+          <BIconCheckSquareFill color="#22a6b3" />
+        </span>
+        <span class="icon" v-else><BIconCheckSquare color="#22a6b3" /></span>
+
+        <a :href="link.url" target="_blank">{{ link.title }}</a>
+
+        <span>{{ link.type === 'video' ? '📺 ' : '📄 ' }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import { links } from '@/constants';
+import { BIconCheckSquareFill, BIconCheckSquare } from 'bootstrap-vue';
 
 export default Vue.extend({
   name: 'Home',
+  components: {
+    BIconCheckSquareFill,
+    BIconCheckSquare,
+  },
   data() {
     return {
-      count: 0,
+      selected: 'all',
+      links,
     };
   },
   methods: {
-    handleClickPlus() {
-      this.count++;
-    },
-    handleClickMinus() {
-      this.count--;
+    handleClick(e: Event) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const type = (e.target as HTMLButtonElement).dataset.type!;
+      this.selected = type;
+      if (type === 'all') this.links = links;
+      else if (type === 'v2')
+        this.links = links.filter((link) => link.version === 2);
+      else this.links = links.filter((link) => link.version === 3);
     },
   },
 });
 </script>
+<style scoped>
+.button-container {
+  margin: 20px 0;
+}
+button {
+  all: unset;
+  width: 100px;
+  padding: 5px 0;
+  text-align: center;
+  border: 1px solid #22a6b3;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: all 0.2s;
+}
+button + button {
+  margin-left: 10px;
+}
+.icon {
+  margin-right: 5px;
+}
+.active {
+  background-color: #22a6b3;
+  color: white;
+  font-weight: 500;
+}
+</style>
