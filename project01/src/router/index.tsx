0b9098b9da160c0a447a8f67c11e54045e40e149
jjanmo <jjanmo07@hanmail.net>
2023-10-04 16:29:38 +0900

    refactor: move router files like vue style

--- project01/src/router/index.tsx
+++ project01/src/router/index.tsx
@@ -5,12 +5,14 @@ import About from '@/views/About.vue';
 
 Vue.use(VueRouter);
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+];
+
 const router = new VueRouter({
   mode: 'history',
-  routes: [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
-  ],
+  routes,
   linkExactActiveClass: 'active',
 });
 
