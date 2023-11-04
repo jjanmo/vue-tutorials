6b3480229f86275fc254cdbfe0cfef01d65ebbb9
jjanmo <jjanmo07@hanmail.net>
2023-11-04 16:43:33 +0900

    feat: add drag board layout for vuedraggable

--- project01/src/components/lol/ChampionList.vue
+++ project01/src/components/lol/ChampionList.vue
@@ -1,57 +1,123 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="champion of champions" :key="champion.key">
-        <img :src="baseUrl + champion.image.full" alt="champion" />
-      </li>
-    </ul>
+  <div v-if="Object.values(champions).length === 0">Loading...</div>
+  <div v-else class="board">
+    <draggable class="row" :list="selectedChampions" itemKey="id">
+      <template #item="{ element }">
+        <div class="droppable">{{ element.name }}</div>
+      </template>
+    </draggable>
+
+    <draggable class="row" :list="shuffle(selectedChampions)" itemKey="id">
+      <template #item="{ element }">
+        <div class="droppable">
+          <img :src="element.image" alt="champion" />
+        </div>
+      </template>
+    </draggable>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { ChampionsResponse, Champions } from '@/store/champion.type';
+import { ChampionsResponse, Champion } from '@/store/champion.type';
+import draggable from 'vuedraggable';
 
-interface Data {
-  champions: Champions | null;
-  ids: string[] | null;
-  baseUrl: string;
+type ImageBaseUrl = 'https://ddragon.leagueoflegends.com/cdn/13.21.1/img/champion/';
+interface SelectedChampion {
+  id: string;
+  name: string;
+  image: `${ImageBaseUrl}${string}`;
 }
 
+const TOTAL = 10;
+const IMAGE_BASE_URL = `https://ddragon.leagueoflegends.com/cdn/13.21.1/img/champion/`;
+
 export default defineComponent({
+  components: {
+    draggable,
+  },
+
   data() {
     return {
-      champions: null,
-      ids: null,
-      baseUrl: 'https://ddragon.leagueoflegends.com/cdn/13.21.1/img/champion/',
-    } as Data;
+      champions: {},
+    };
   },
   mounted() {
     this.fetchChampions();
   },
   methods: {
     async fetchChampions() {
-      const response = await fetch(
-        `http://ddragon.leagueoflegends.com/cdn/13.21.1/data/ko_KR/champion.json`
-      );
+      const response = await fetch(`http://ddragon.leagueoflegends.com/cdn/13.21.1/data/ko_KR/champion.json`);
       const result: ChampionsResponse = await response.json();
       this.champions = result.data;
-      const ids = Object.keys(result.data);
-      this.ids = ids;
+    },
+
+    // TODO 로직 최적화
+    shuffle(array: SelectedChampion[]) {
+      const result: SelectedChampion[] = [];
+
+      let index = 0;
+      while (result.length < TOTAL) {
+        const selectedIndex = Math.floor(Math.random() * TOTAL);
+
+        const isAlreadySelected = result.find((champion) => champion.id === array[selectedIndex].id); // 이미 뽑은것인가
+        const isSameIndex = index === selectedIndex; // 현재 뽑을려고 하는 자리와 같은 자리인가
+
+        if ((!isAlreadySelected && !isSameIndex) || (index === 9 && !isAlreadySelected)) {
+          result.push(array[selectedIndex]);
+          index++;
+        }
+      }
+
+      return result;
+    },
+  },
+
+  computed: {
+    selectedChampions() {
+      const championsArr: Champion[] = Object.values(this.champions);
+      if (championsArr.length > 0) {
+        const result: SelectedChampion[] = [];
+
+        while (result.length < TOTAL) {
+          const index = Math.floor(Math.random() * championsArr.length);
+          const selected = championsArr[index];
+
+          const finded = result.find((champion) => champion.id === selected.id);
+
+          if (!finded)
+            result.push({
+              id: selected.id,
+              image: `${IMAGE_BASE_URL}${selected.image.full}`,
+              name: selected.name,
+            });
+        }
+        return result;
+      }
+
+      return [];
     },
   },
 });
 </script>
 
 <style scoped>
-ul {
-  display: grid;
-  grid-template-columns: repeat(5, 100px);
-  gap: 20px;
-  justify-content: center;
+.board {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
 }
-img {
-  width: 100px;
-  height: 100px;
+.row {
+  display: grid;
+  grid-template-columns: repeat(2, 120px);
+  grid-template-rows: repeat(5, 120px);
+  gap: 10px;
+}
+.droppable {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #eee;
 }
 </style>
