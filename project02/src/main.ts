2c790fb3be5b319182b60a7f4528c09857979148
jjanmo <jjanmo07@hanmail.net>
2023-11-07 23:37:59 +0900

    feat: apply global reset style

--- project02/src/main.ts
+++ project02/src/main.ts
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/router';
 
-Vue.config.productionTip = false
+// TODO : 다른 방법으로 글로벌 스타일 적용법 찾아보기
+import '@/styles/_reset.scss';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
