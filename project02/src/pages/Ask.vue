6e4ea55e499755ae2ce4d389226e8698403e2049
jjanmo <jjanmo07@hanmail.net>
2023-11-08 00:02:01 +0900

    feat: add routes

---
+++ project02/src/pages/Ask.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>This is Ask Page ✅</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class New extends Vue {}
+</script>
+
+<style scoped></style>
--- project02/src/router/index.ts
+++ project02/src/router/index.ts
@@ -1,5 +1,7 @@
 import Home from '@/pages/Home.vue';
 import New from '@/pages/New.vue';
+import Ask from '@/pages/Ask.vue';
+import Jobs from '@/pages/Jobs.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
@@ -11,6 +13,14 @@ const routes = [
   {
     path: '/new',
     component: New,
+  },
+  {
+    path: '/ask',
+    component: Ask,
+  },
+  {
+    path: '/jobs',
+    component: Jobs,
   },
 ];
 
