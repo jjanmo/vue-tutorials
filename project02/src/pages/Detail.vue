366dc2dae9fc9131231d9c5479f1c9cb520bac79
jjanmo <jjanmo07@hanmail.net>
2023-11-23 23:53:08 +0900

    feat: update ask detail page style

--- project02/src/pages/Detail.vue
+++ project02/src/pages/Detail.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>Item Vue</div>
+  <div v-if="detailedItem" class="item-container">
+    <div class="title">{{ detailedItem.title }}</div>
+    <div class="item-info">
+      {{ detailedItem.points }} points by {{ detailedItem.user }} {{ detailedItem.time_ago }} |
+      {{ detailedItem.comments_count }} comments
+    </div>
+    <p class="content" v-html="detailedItem.content"></p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,16 +15,42 @@ import { getDetailedItem } from '@/api';
 import { DetailedItem } from '@/interface/list';
 
 @Component
-/** Ask의 상세 페이지 */
 export default class Detail extends Vue {
   detailedItem: DetailedItem | null = null;
+  isLoading = false;
 
   async mounted() {
-    const { id } = this.$route.query;
-    const { data } = await getDetailedItem(id as string);
-    this.detailedItem = data;
+    try {
+      this.isLoading = true;
+      const { id } = this.$route.query;
+      const { data } = await getDetailedItem(id as string);
+      this.detailedItem = data;
+    } catch (e) {
+      console.error(e);
+    } finally {
+      this.isLoading = false;
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.item-container {
+  padding: 20px 15px;
+  margin: 20px 0;
+  background-color: white;
+}
+.title {
+  font-size: 18px;
+  color: #2c3e50;
+}
+.item-info {
+  margin-top: 5px;
+  font-size: 14px;
+  color: #95a5a6;
+}
+.content {
+  margin-top: 20px;
+  line-height: 1.4rem;
+}
+</style>
