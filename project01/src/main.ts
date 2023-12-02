59ecd85abf59267708f4c93b485b087ef42eb093
jjanmo <jjanmo07@hanmail.net>
2023-12-03 02:18:32 +0900

    feat: add vue-portal for modal

--- project01/src/main.ts
+++ project01/src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router';
+import PortalVue from 'portal-vue';
 import store from '@/store';
 import '@/styles/global.css';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(store) //
+  .use(router)
+  .use(PortalVue)
+  .mount('#app');
