35ac6e393fdba1937b306d4c38eb395d67214f07
jjanmo <jjanmo07@hanmail.net>
2023-12-03 02:18:53 +0900

    feat: add modal comp

---
+++ project01/src/components/common/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="modal">
+    <div class="overlay" @click="handleCloseModal"></div>
+    <div class="modal-content">
+      <portal-target name="modal-content"></portal-target>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  methods: {
+    handleCloseModal() {
+      this.$store.commit('modal/closeModal');
+    },
+  },
+});
+</script>
+
+<style scoped>
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+}
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal-content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 5px;
+  width: 280px;
+  min-height: 200px;
+  padding: 20px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
