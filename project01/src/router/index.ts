f140f9cb418e07aa1213454838e2570f39107648
jjanmo <jjanmo07@hanmail.net>
2024-02-29 23:08:40 +0900

    feat: 라이트갤러리 페이지 추가

--- project01/src/router/index.ts
+++ project01/src/router/index.ts
@@ -3,12 +3,14 @@ import Home from '@/views/Home.vue';
 import TodoApp from '@/views/TodoApp.vue';
 import MathcingGame from '@/views/MatchingGame.vue';
 import MemoryGame from '@/views/MemoryGame.vue';
+import Gallery from '@/views/Gallery.vue';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', name: 'home', component: Home },
   { path: '/todoapp', name: 'todoapp', component: TodoApp },
   { path: '/matching', name: 'matching', component: MathcingGame },
   { path: '/memory', name: 'memory', component: MemoryGame },
+  { path: '/gallery', name: 'gallery', component: Gallery },
 ];
 
 const router = createRouter({
