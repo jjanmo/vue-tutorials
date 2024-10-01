b95740964094874d0ff19ef5aee04a5b6b50295b
jjanmo <jjanmo07@hanmail.net>
2024-10-02 01:45:10 +0900

    fix: 리턴타입 추가

--- project01/src/utils/index.ts
+++ project01/src/utils/index.ts
@@ -3,7 +3,7 @@
  * @param count : number of elements to select, default is array length
  * @returns : shuffled array
  */
-export function shuffleAndSelect<T>(array: T[], count = array.length) {
+export function shuffleAndSelect<T>(array: T[], count = array.length): T[] {
   for (let i = array.length - 1; i > 0; i--) {
     const randomIndex = Math.floor(Math.random() * (i + 1));
     [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
